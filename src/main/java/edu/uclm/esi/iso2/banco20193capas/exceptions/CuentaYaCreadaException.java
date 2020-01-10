c5021b9261925c24a3b3560f66955a6c24b8e06d
Francisco Javier Garcia <fcojavier.garcia4@alu.uclm.es>
2020-01-11 00:23:10 +0100

    Corregido error clase CuentaYaCreadaException

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/CuentaYaCreadaException.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/CuentaYaCreadaException.java
@@ -1,7 +1,7 @@
 package edu.uclm.esi.iso2.banco20193capas.exceptions;
 
 public class CuentaYaCreadaException extends Exception {
-	static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	public CuentaYaCreadaException() {
 		super("La cuenta está creada y no admite añadir titulares");
 	}
