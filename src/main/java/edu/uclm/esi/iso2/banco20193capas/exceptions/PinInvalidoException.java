30cbdea69b230986c5410692f57f43e6f178f3f6
Francisco Javier Garcia <fcojavier.garcia4@alu.uclm.es>
2020-01-11 00:26:18 +0100

    Corregido error clase PinInvalidoException

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/PinInvalidoException.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/PinInvalidoException.java
@@ -1,7 +1,7 @@
 package edu.uclm.esi.iso2.banco20193capas.exceptions;
 
 public class PinInvalidoException extends Exception {
-	static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	public PinInvalidoException() {
 		super("PIN inválido");
 	}
