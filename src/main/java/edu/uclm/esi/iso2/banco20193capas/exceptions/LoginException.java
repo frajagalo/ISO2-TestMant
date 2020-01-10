933df53c5d4b57bc0e2036f8928434cac171134c
Francisco Javier Garcia <fcojavier.garcia4@alu.uclm.es>
2020-01-11 00:25:03 +0100

    Corregido error clase LoginException

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/LoginException.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/LoginException.java
@@ -1,7 +1,7 @@
 package edu.uclm.esi.iso2.banco20193capas.exceptions;
 
 public class LoginException extends Exception { 
-	static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	public LoginException() {
 		super("Credenciales inválidas");
 	}
