2b76a187a5f6cde9945e3fc87a50e5b5ed31bb03
Francisco Javier Garcia <fcojavier.garcia4@alu.uclm.es>
2020-01-11 00:23:30 +0100

    Corregido error clase ImporteInvalidoException

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/ImporteInvalidoException.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/ImporteInvalidoException.java
@@ -1,8 +1,8 @@
 package edu.uclm.esi.iso2.banco20193capas.exceptions;
 
 public class ImporteInvalidoException extends Exception {
-	static final long serialVersionUID = 1L;
-	public ImporteInvalidoException(double importe) {
+	private static final long serialVersionUID = 1L;
+	public ImporteInvalidoException(final double importe) {
 		super("El importe " + importe + " no es válido para esta operación");
 	}
 }
